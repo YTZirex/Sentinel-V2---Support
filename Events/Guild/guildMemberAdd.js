036a08a287c674334771d9e3a9e277aadaff4ea3
OFB <ofb_bbx@proton.me>
2023-12-20 19:34:38 +0100

    fix

--- Events/Guild/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-const { EmbedBuilder, GuildMember } = require("discord.js");
-
-module.exports = {
-  name: "guildMemberAdd",
-  async execute(member) {
-   // const role = member.guild.roles.cache.get('774713026867757056')
-    member.roles.add("1159171919237627945");
-  },
-};
--- Models/Welcome.js
+++ Models/Welcome.js
@@ -2,6 +2,7 @@ const { model, Schema } = require("mongoose");
 
 let welcomeSchema = new Schema({
   guild: String,
+  enabled: Boolean, 
   channel: String,
   message: String,
   role: String,
