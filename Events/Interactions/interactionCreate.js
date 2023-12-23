00aa85283f0527b605163f29a39c462a53772710
OFB <ofb_bbx@proton.me>
2023-12-23 17:00:32 +0100

    added premium only to interactioncreate

--- Events/Interactions/interactionCreate.js
+++ Events/Interactions/interactionCreate.js
@@ -2,7 +2,7 @@ const { CommandInteraction, EmbedBuilder } = require("discord.js");
 const { models, Schema } = require("mongoose");
 const BlacklistSchema = require("../../Models/Blacklist");
 const Code = require('../../Models/CodeSchema');
-//const isUserPremium = require("../../Functions/codeFunction");
+
 module.exports = {
   name: "interactionCreate",
 
@@ -20,51 +20,56 @@ module.exports = {
       user: interaction.user.id,
     });
 
-    if (userBlacklistRecord) {
-      if (userBlacklistRecord.blacklisted == true) {
-        const res = new EmbedBuilder()
-          .setTitle("Oups!")
-          .setColor("Red")
-          .setThumbnail(interaction.user.displayAvatarURL())
-          .setDescription(
-            `Vous êtes actuellement blacklist de nos services. Si vous pensez que c'est une erreur, veuillez nous contacter sur notre [Support](https://discord.gg/My2BVCmJEY).`
-          )
-          .addFields(
-            {
-              name: "**Raison : **",
-              value: userBlacklistRecord.blacklistReason,
-            },
-            {
-              name: "**Modérateur : **",
-              value: `<@${userBlacklistRecord.author}>`,
-            }
-          )
-          .setTimestamp();
-
-        interaction.reply({
-          embeds: [res],
-          ephemeral: true,
-        });
-        return;
-      }
-    }
-
-    const userPremiumRecord = await Code.findOne({
-      "redeemedBy.id": interaction.user.id
-    })
-
-    if (command.premiumOnly && !userPremiumRecord) {
+    if (userBlacklistRecord && userBlacklistRecord.blacklisted) {
       const res = new EmbedBuilder()
         .setTitle("Oups!")
-        .setDescription(
-          `Cette commande est réservé aux utilisateurs Sentinel Premium.`
-        )
         .setColor("Red")
+        .setThumbnail(interaction.user.displayAvatarURL())
+        .setDescription(
+          `Vous êtes actuellement blacklisté de nos services. Si vous pensez que c'est une erreur, veuillez nous contacter sur notre [Support](https://discord.gg/My2BVCmJEY).`
+        )
+        .addFields(
+          {
+            name: "**Raison : **",
+            value: userBlacklistRecord.blacklistReason,
+          },
+          {
+            name: "**Modérateur : **",
+            value: `<@${userBlacklistRecord.author}>`,
+          }
+        )
         .setTimestamp();
-      return interaction.reply({
+
+      interaction.reply({
         embeds: [res],
         ephemeral: true,
       });
+      return;
+    }
+
+    if (command.premiumOnly) {
+      const userPremiumRecord = await Code.findOne({
+        "redeemedBy.id": interaction.user.id,
+      });
+
+      if (!userPremiumRecord || userPremiumRecord.expiresAt < new Date()) {
+        if (userPremiumRecord) {
+          // Remove the expired code from the database
+          await Code.deleteOne({ "redeemedBy.id": interaction.user.id });
+        }
+
+        const res = new EmbedBuilder()
+          .setTitle("Oups!")
+          .setDescription(
+            `Cette commande est réservée aux utilisateurs Sentinel Premium.`
+          )
+          .setColor("Red")
+          .setTimestamp();
+        return interaction.reply({
+          embeds: [res],
+          ephemeral: true,
+        });
+      }
     }
 
     command.execute(interaction, client);
